043fbb7b7d7e3f4a563ee636f03a01ee94f7cf89
Rhys Walsh-Tindall <rhys_walsh@hotmail.com>
2015-09-10 17:06:30 +1000

    Added example keys config file.

---
+++ config/keysExample.js
@@ -0,0 +1,8 @@
+var accessKeys = {
+	
+	'client_id': '<insert your key here>', // Your client id
+	'client_secret': '<insert your key here>', // Your client secret
+	'redirect_uri': 'http://localhost:8888/callback' // Your redirect uri (replace if required)
+};
+
+module.exports = accessKeys;
