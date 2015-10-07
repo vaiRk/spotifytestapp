47e93d9fba20930e7f492552baea53aab22f97d9
Rhys Walsh-Tindall <rhys_walsh@hotmail.com>
2015-10-07 18:01:39 +1000

    Animating the footer info bar with song and playlist information.

--- public/js/events.js
+++ public/js/events.js
@@ -33,6 +33,7 @@ var applyEventListeners = function() {
 			var mergeButton = $("#merge-button");
 			var albumCount = $("#album-count");
 			var trackCount = $("#track-count");
+			var footerInfo = $("#footer-info");
 			var dataTrackCount = parseInt($( this ).find('img').data('trackcount'));
 		
 			if ( $( this ).find('.selected').length ) {
@@ -47,6 +48,7 @@ var applyEventListeners = function() {
 				
 				if (count == 0 || trackCountVal == 0) {
 					mergeButton.prop('disabled', 'disabled');
+					footerInfo.animate( { height:"0px" }, { queue:false, duration:500 });
 				}
 												
 			} else {
@@ -55,6 +57,7 @@ var applyEventListeners = function() {
 				$( this ).children('a').append( $("<div class='selected'><table><tr><td>Selected!</td></tr>" +
 				"<tr><td><span class='glyphicon glyphicon-minus' aria-hidden='true'></span></td></tr></table></div>" ) );								
 				
+				//albumCount.text() will be '0' by default
 				if (albumCount.text().length) {
 					var count = parseInt(albumCount.text()) + 1;
 					albumCount.text(count);
@@ -62,9 +65,10 @@ var applyEventListeners = function() {
 					var trackCountVal = parseInt(trackCount.text()) + dataTrackCount;
 					trackCount.text(trackCountVal);
 					
-				} else {
-					albumCount.text('1');
-					trackCount.text(dataTrackCount);
+					footerInfo.animate( { height:"0px" }, { queue:false, duration:0 });					
+					footerInfo.prop('hidden', false);
+					footerInfo.animate( { height:"70px" }, { queue:false, duration:500 });
+					
 				}
 				
 				mergeButton.prop('disabled', false);
